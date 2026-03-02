e1022c8f8fe9da5572f60aa3304317244a702999
isabelle bidou <isa.bidou@gmail.com>
2026-03-02 14:29:54 +0100

    providing feedback on answers after the test

--- client/src/components/UserExercice.js
+++ client/src/components/UserExercice.js
@@ -15,6 +15,7 @@ function UserExercice({ grammarTopics = [], fetchGrammarTopics }) {
   const [finished, setFinished] = useState(false)
   const [readyForNext, setreadyForNext] = useState(false)
   const [selectedAnswer, setSelectedAnswer] = useState(null)
+  const [feedback, setFeedBack] = useState([])
 
   useEffect(() => {
     fetchGrammarTopics()
@@ -40,20 +41,45 @@ function UserExercice({ grammarTopics = [], fetchGrammarTopics }) {
     } finally {
     }
   }
+  const findCorrectAnswer = (q) => {
+    for (let index = 0; index < q.options.length; index++) {
+      const option = q.options[index]
+      if (option.isCorrect) return option
+    }
+  }
 
   const displayNext = () => {
     if (!selectedAnswer) return
-
+    const correct = findCorrectAnswer(questions[answeredQuestion])
     if (selectedAnswer.isCorrect) {
       setScore((prev) => prev + 1)
+      setFeedBack((prev) => [
+        ...prev,
+        {
+          question: questions[answeredQuestion].sentence,
+          wrongAnswer: selectedAnswer.text,
+          correctAnswer: selectedAnswer.text,
+          isCorrect: true,
+        },
+      ])
+    } else {
+      setFeedBack((prev) => [
+        ...prev,
+        {
+          question: questions[answeredQuestion].sentence,
+          wrongAnswer: selectedAnswer.text,
+          correctAnswer: correct.text,
+          isCorrect: false,
+        },
+      ])
     }
-
     setSelectedAnswer(null)
     setreadyForNext(false)
 
-    if (answeredQuestion === questions.length-1) {
+    if (answeredQuestion === questions.length - 1) {
       setGrade((score * 100) / questions.length)
       setFinished((prev) => !prev)
+      console.log(feedback, null, 2)
     } else {
       setAnsweredQuestion((prev) => prev + 1)
     }
@@ -66,28 +92,79 @@ function UserExercice({ grammarTopics = [], fetchGrammarTopics }) {
   return (
     <div className="page">
       <div className="exercice-div">
-      <SelectTopic className= "exercice-select"
-        topics={grammarTopics}
-        selectedTopic={selectedTopic}
-        onChange={setSelectedTopic}
-      />
-      <button onClick={handleClick}>Generate Exercice</button>
-      {/* once exercice is generated and started */}
-      {gameStarted && questions.length > 0 && (
-        <QuestionBundle
-          questionIndex={answeredQuestion}
-          question={questions[answeredQuestion]}
-          selectedAnswer={selectedAnswer}
-          onSelectAnswer={handleSelectAnswer}
-        ></QuestionBundle>
-      )}
-      {/* once one option is selected */}
-      {gameStarted && selectedAnswer && (
-        <button className="next-button" onClick={displayNext}>Next</button>
-      )}
+        {!gameStarted && (
+          <SelectTopic
+            className="exercice-select"
+            topics={grammarTopics}
+            selectedTopic={selectedTopic}
+            onChange={setSelectedTopic}
+          />
+        )}
+        {!gameStarted && selectedTopic && (
+          <button onClick={handleClick}>Generate Exercice</button>
+        )}
+        {/* once exercice is generated and started */}
+        {gameStarted &&  selectedTopic && questions.length > 0 && (
+          <h2>{selectedTopic.name}</h2>
+          )}
 
-      {/* once finished */}
-      {finished && <div>`{grade}/100`</div>}
+        {gameStarted &&  questions.instructions && questions.length > 0 && (
+          <p>{questions.instructions}</p>
+          )}
+
+        {gameStarted && questions.length > 0 && (
+          <QuestionBundle
+            questionIndex={answeredQuestion}
+            question={questions[answeredQuestion]}
+            selectedAnswer={selectedAnswer}
+            onSelectAnswer={handleSelectAnswer}
+          ></QuestionBundle>
+        )}
+        {/* once one option is selected */}
+        {gameStarted && selectedAnswer && (
+          <button className="next-button" onClick={displayNext}>
+            Next
+          </button>
+        )}
+
+        {/* once finished */}
+
+        {finished && (
+          <div className="exercicResultDiv">
+            <div>{grade}/100</div>
+            <table className="resultTable">
+              <tr>
+                <th>
+                  <strong>Question:</strong>
+                </th>
+                <th>
+                  <strong>Your answer:</strong>
+                </th>
+                <th>
+                  <strong>Correct answer:</strong>
+                </th>
+                <th>
+                  <strong></strong>
+                </th>
+              </tr>
+              {feedback.map((f, index) => (
+                <tr key={index}>
+                  <td>{f.question}</td>
+                  <td>{f.wrongAnswer}</td>
+                  <td>{f.correctAnswer}</td>
+                  <td>{f.correctAnswer}</td>
+                  <td>
+                    {f.isCorrect ? (
+                      <span style={{ color: "green" }}>✓</span>
+                    ) : (
+                      <span style={{ color: "red" }}>✘</span>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </table>
+          </div>
+        )}
       </div>
     </div>
   )
