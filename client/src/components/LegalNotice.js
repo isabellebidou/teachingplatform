a6f0fe5ea58b58226ff64ac57ee2b530afa0e679
isabelle bidou <isa.bidou@gmail.com>
2026-08-21 18:35:16 +0200

    fixed legal notice visibility issue

--- client/src/components/LegalNotice.js
+++ client/src/components/LegalNotice.js
@@ -1,64 +1,81 @@
-
-
 const LegalNotice = () => {
+  return (
+    <div className="page">
+      <fieldset>
+        <legend>Legal Notice</legend>
+        <p className="itemp">
+          In accordance with the provisions of Law No. 2004-575 of June 21, 2004
+          on confidence in the digital economy, users of the Izzy Speak English
+          site are informed of the identity of the various parties involved in
+          its implementation and monitoring.
+        </p>
+        <h2>Editing the site</h2>
+        <p className="itemp">
+          This site, accessible at the URL{" "}
+          <a href="https://izzyspeakenglish.com/">
+            https://izzyspeakenglish.com/
+          </a>{" "}
+          (the “Site”), is published by:
+          <br />
+          Isabelle Bidou, residing at 52 avenue de Choisy, 75013 Paris, France -
+          of French nationality (France), born on 28/10/1971, The publisher's
+          individual Siren number is: 103664934.
+        </p>
+        <h2>Accommodation</h2>
+        <p className="itemp">
+          The Site is hosted by Railway, (telephone or email contact: Phone:
+          (415) 707-7675; Address: 548 Market St PMB 68956, San Francisco,
+          California 94104).
+        </p>
+        <h2>Publication director</h2>
+        <p className="itemp">
+          The Publication Director of the Site is Isabelle Bidou.
+        </p>
+        <h2>Contact us</h2>
+        <p className="itemp">
+          By phone: +33 (0)753672587
+          <br />
+          By email : isa.bidou@gmail.com
+          <br />
+          By mail: 52 avenue de Choisy, 75013 Paris, France
+          <br />
+        </p>
+        <h2>Personal data</h2>
+        <p className="itemp">
+          The company provides services including the teaching of languages.
+        </p>
+        <h2>Terms of Sales</h2>
+        <p className="itemp">
+          The company provides services including the teaching of languages.
+        </p>
+        <h2>
+          Delivery delay: Once payment is completed we agree on Coaching
+          sessions.
+        </h2>
 
+        <h2>Terms of payment:</h2>
+        <p className="itemp">
+          Payment Terms: Single sessions and single workshops are payable in
+          advance at the time of booking. Pilot programmes are invoiced either
+          in full before the programme begins or in two instalments: 50% upon
+          acceptance of the quotation and 50% upon completion. Monthly
+          programmes are invoiced at the beginning of each month and payable
+          within 30 days by bank transfer.
+        </p>
+        <h2>Termination Policy:</h2>
+        <p className="itemp">
+          Monthly individual coaching programmes may be terminated by either
+          party with 30 days' written notice. Corporate monthly workshop
+          programmes are subject to an initial six-month commitment period.
+          After this period, either party may terminate the programme with 30
+          days' written notice. The trainer reserves the right to suspend or
+          terminate services in the event of non-payment or circumstances
+          preventing the proper delivery of the programme.
+        </p>
+      </fieldset>
+      <div className="placeholder"></div>
+    </div>
+  )
+}
 
-
-    return (
-        <div className='page'>
-            <h1>Legal Notice</h1>
-            <p className='itemp'>
-                In accordance with the provisions of Law No. 2004-575 of June 21, 2004 on confidence in the digital economy, users of the Izzy Speak English site are informed of the identity of the various parties involved in its implementation and monitoring.
-            </p>
-            <h2>Editing the site</h2>
-            <p className='itemp'>
-                This site, accessible at the URL <a href="https://izzyspeakenglish.com/">https://izzyspeakenglish.com/</a> (the “Site”), is published by:<br />
-
-                Isabelle Bidou, residing at 52 avenue de Choisy, 75013 Paris,  France -  of French nationality (France), born on 28/10/1971, 
-
-                The publisher's individual Siren number is: 103664934.
-            </p>
-            <h2>Accommodation</h2>
-            <p className='itemp'>
-                The Site is hosted by Railway, (telephone or email contact: Phone: (415) 707-7675; Address: 548 Market St PMB 68956, San Francisco, California 94104).
-
-            </p>
-            <h2>Publication director</h2>
-            <p className='itemp'>The Publication Director of the Site is Isabelle Bidou.
-            </p>
-            <h2>Contact us</h2>
-            <p className='itemp'>By phone: +33 (0)753672587<br />
-                By email : isa.bidou@gmail.com<br />
-                By mail: 52 avenue de Choisy, 75013 Paris, France<br />
-            </p>
-            <h2>Personal data</h2>
-            <p className='itemp'>The company provides services including the teaching of languages.
-            </p>
-            <h2>Terms of Sales</h2>
-            <p className='itemp'>The company provides services including the teaching of languages.
-            </p>
-            <h2>Delivery delay: Once payment is completed we agree on Coaching sessions.
-            </h2>
-            
-            <h2>Terms of payment:
-            </h2>
-            <p className='itemp'>Payment Terms:
-Single sessions and single workshops are payable in advance at the time of booking.
-
-Pilot programmes are invoiced either in full before the programme begins or in two instalments: 50% upon acceptance of the quotation and 50% upon completion.
-
-Monthly programmes are invoiced at the beginning of each month and payable within 30 days by bank transfer.
-            </p>
-            <h2>Termination Policy:
-            </h2>
-            <p className='itemp'>Monthly individual coaching programmes may be terminated by either party with 30 days' written notice.
-
-Corporate monthly workshop programmes are subject to an initial six-month commitment period. After this period, either party may terminate the programme with 30 days' written notice.
-
-The trainer reserves the right to suspend or terminate services in the event of non-payment or circumstances preventing the proper delivery of the programme.
-    </p>
-        </div>
-    );
-};
-
-export default LegalNotice;
+export default LegalNotice
